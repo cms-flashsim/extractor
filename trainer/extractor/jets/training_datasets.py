f62c9d370e5b57d6cebe3241e87ed0e8ad04c628
fcattafesta <filippo.cattafesta99@gmail.com>
2023-05-18 15:44:02 +0200

    import fix

--- trainer/extractor/jets/training_datasets.py
+++ trainer/extractor/jets/training_datasets.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "preprocessing)"))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "preprocessing"))
 
 from preprocessing import make_dataset
 from prep_actions import target_dictionary_jets as target_dictionary
@@ -17,4 +17,12 @@ inputtrees = [
     f"{os.path.join(os.path.dirname(__file__), 'dataset', f)}:MJets" for f in datasets
 ]
 
-make_dataset(inputtrees, "MJets", target_dictionary, scale_file, range_file, jet_cond, reco_columns)
+make_dataset(
+    inputtrees,
+    "MJets",
+    target_dictionary,
+    scale_file,
+    range_file,
+    jet_cond,
+    reco_columns,
+)

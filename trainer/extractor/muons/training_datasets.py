4425cae893a697998739104979e8edea075a0a95
fcattafesta <filippo.cattafesta99@gmail.com>
2023-05-19 14:03:39 +0200

    muon dataset

--- trainer/extractor/muons/training_datasets.py
+++ trainer/extractor/muons/training_datasets.py
@@ -11,13 +11,18 @@ from columns import muon_cond, reco_columns
 datasets = os.listdir(os.path.join(os.path.dirname(__file__), "dataset"))
 
 scale_file = os.path.join(os.path.dirname(__file__), "scale_factors_muons.json")
+range_file = os.path.join(os.path.dirname(__file__), "ranges_muons.json")
 
 inputtrees = [
     f"{os.path.join(os.path.dirname(__file__), 'dataset', f)}:MMuons" for f in datasets
 ]
 
-scale_file = os.path.join(os.path.dirname(__file__), "scale_factors_muons.json")
-
 make_dataset(
-    inputtrees, "MMuons", target_dictionary, scale_file, muon_cond, reco_columns
+    inputtrees,
+    "MMuons",
+    target_dictionary,
+    scale_file,
+    range_file,
+    muon_cond,
+    reco_columns,
 )

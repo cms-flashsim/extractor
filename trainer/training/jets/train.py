2920c0410a05dd1dd791ce7d38168429d4c68eca
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-05-24 14:13:58 +0200

    print when loss i snone

--- trainer/training/jets/train.py
+++ trainer/training/jets/train.py
@@ -277,7 +277,9 @@ def trainer(gpu, save_dir, ngpus_per_node, args, val_func):
                             loss.item(),
                         )
                     )
-            else: print("Loss is nan or inf for this batch")
+            else: 
+                print("Loss is nan or inf for this batch")
+                print(log_p, log_det, loss)
 
         train_loss = (train_loss.item() / len(train_loader.dataset)) * args.world_size
         train_log_p = (train_log_p.item() / len(train_loader.dataset)) * args.world_size

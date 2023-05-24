a9e2438bb7f1cc8ebb9338a0555f79bc286d6ef1
fcattafesta <filippo.cattafesta99@gmail.com>
2023-05-24 10:21:33 +0200

    Added training dirs

--- trainer/extractor/electrons/training_dataset.py
+++ trainer/extractor/electrons/training_dataset.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "preprocessing"
 
 from preprocessing import make_dataset
 
-from columns import gen_ele, gen_pho, gen_jet
+from columns import gen_ele, gen_pho, gen_jet, reco_columns
 
 from prep_actions_ele import target_dictionary as target_dictionary_ele
 from prep_actions_pho import target_dictionary as target_dictionary_pho
@@ -21,9 +21,18 @@ filenames = [f"MElectrons_{i}_ele.root:MElectrons" for i in range(nfiles)]
 
 filepaths = [os.path.join(dataset_path, f) for f in filenames]
 
-scale_name = "scale_factors_ele.json"
+scale_file = os.path.join(os.path.dirname(__file__), "scale_factors_ele_ele.json")
+range_file = os.path.join(os.path.dirname(__file__), "ranges_ele_ele.json")
 
-make_dataset(filepaths, "MElectrons_ele", target_dictionary_ele, scale_name, gen_ele)
+make_dataset(
+    filepaths,
+    "MElectrons_ele",
+    target_dictionary_ele,
+    scale_file,
+    range_file,
+    gen_ele,
+    reco_columns,
+)
 
 
 # GenPhoton training dataset
@@ -32,9 +41,18 @@ filenames = [f"MElectrons_{i}_pho.root:MElectrons" for i in range(nfiles)]
 
 filepaths = [os.path.join(dataset_path, f) for f in filenames]
 
-scale_name = "scale_factors_pho.json"
+scale_file = os.path.join(os.path.dirname(__file__), "scale_factors_ele_pho.json")
+range_file = os.path.join(os.path.dirname(__file__), "ranges_ele_pho.json")
 
-make_dataset(filepaths, "MElectrons_pho", target_dictionary_pho, scale_name, gen_pho)
+make_dataset(
+    filepaths,
+    "MElectrons_pho",
+    target_dictionary_ele,
+    scale_file,
+    range_file,
+    gen_pho,
+    reco_columns,
+)
 
 
 # GenJet training dataset
@@ -43,6 +61,15 @@ filenames = [f"MElectrons_{i}_jet.root:MElectrons" for i in range(nfiles)]
 
 filepaths = [os.path.join(dataset_path, f) for f in filenames]
 
-scale_name = "scale_factors_jet.json"
+scale_file = os.path.join(os.path.dirname(__file__), "scale_factors_ele_jet.json")
+range_file = os.path.join(os.path.dirname(__file__), "ranges_ele_jet.json")
 
-make_dataset(filepaths, "MElectrons_jet", target_dictionary_jet, scale_name, gen_jet)
+make_dataset(
+    filepaths,
+    "MElectrons_jet",
+    target_dictionary_ele,
+    scale_file,
+    range_file,
+    gen_jet,
+    reco_columns,
+)

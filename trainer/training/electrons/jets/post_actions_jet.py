fd5e0973c06ab16b50e3593d016af35de64d2ac4
fcattafesta <filippo.cattafesta99@gmail.com>
2023-05-24 17:14:54 +0200

    Validation bug

--- trainer/training/electrons/jets/post_actions_jet.py
+++ trainer/training/electrons/jets/post_actions_jet.py
@@ -19,13 +19,22 @@ target_dictionary = {
     "Electron_charge": [["c", 0, [-1, 1]]],
     "Electron_convVeto": [["d", None, None]],
     "Electron_deltaEtaSC": [["i", np.tan, [10, 0]]],
-    "Electron_dr03EcalRecHitSumEt": [["d", [-np.inf, -2], np.log(1e-3)], ["i", np.exp, [1, 1e-3]]],
-    "Electron_dr03HcalDepth1TowerSumEt": [
-        ["d", [-np.inf, -2]],
+    "Electron_dr03EcalRecHitSumEt": [
+        ["d", [-np.inf, -2], np.log(1e-3)],
         ["i", np.exp, [1, 1e-3]],
     ],
-    "Electron_dr03TkSumPt": [["d", [-np.inf, -2], np.log(1e-3)], ["i", np.exp, [1, 1e-3]]],
-    "Electron_dr03TkSumPtHEEP": [["d", [-np.inf, -2], np.log(1e-3)], ["i", np.exp, [1, 1e-3]]],
+    "Electron_dr03HcalDepth1TowerSumEt": [
+        ["d", [-np.inf, -2], np.log(1e-3)],
+        ["i", np.exp, [1, 1e-3]],
+    ],
+    "Electron_dr03TkSumPt": [
+        ["d", [-np.inf, -2], np.log(1e-3)],
+        ["i", np.exp, [1, 1e-3]],
+    ],
+    "Electron_dr03TkSumPtHEEP": [
+        ["d", [-np.inf, -2], np.log(1e-3)],
+        ["i", np.exp, [1, 1e-3]],
+    ],
     "Electron_dxy": [["i", np.tan, [10, 0]]],
     "Electron_dxyErr": [["i", np.exp, [1, 1e-3]]],
     "Electron_dz": [["i", np.tan, [10, 0]]],
@@ -39,8 +48,14 @@ target_dictionary = {
     "Electron_jetPtRelv2": [["i", np.expm1, [1, 0]]],
     "Electron_jetRelIso": [["i", np.exp, [10, 1e-2]]],
     "Electron_lostHits": [["d", None, None]],
-    "Electron_miniPFRelIso_all": [["d", [-np.inf, -5.5], np.log(1e-3)], ["i", np.exp, [1, 1e-3]]],
-    "Electron_miniPFRelIso_chg": [["d", [-np.inf, -5.5], np.log(1e-3)], ["i", np.exp, [1, 1e-3]]],
+    "Electron_miniPFRelIso_all": [
+        ["d", [-np.inf, -5.5], np.log(1e-3)],
+        ["i", np.exp, [1, 1e-3]],
+    ],
+    "Electron_miniPFRelIso_chg": [
+        ["d", [-np.inf, -5.5], np.log(1e-3)],
+        ["i", np.exp, [1, 1e-3]],
+    ],
     "Electron_mvaFall17V1Iso": [],
     "Electron_mvaFall17V1Iso_WP80": [["d", None, None]],
     "Electron_mvaFall17V1Iso_WP90": [["d", None, None]],
@@ -62,7 +77,10 @@ target_dictionary = {
         ["d", [-np.inf, -5.5], np.log(1e-3)],
         ["i", np.exp, [1, 1e-3]],
     ],
-    "Electron_pfRelIso03_chg": [["d", [-np.inf, -5.5], np.log(1e-3) ], ["i", np.exp, [1, 1e-3]]],
+    "Electron_pfRelIso03_chg": [
+        ["d", [-np.inf, -5.5], np.log(1e-3)],
+        ["i", np.exp, [1, 1e-3]],
+    ],
     "Electron_phiMinusGen": [["i", np.tan, [10, 0]]],
     "Electron_ptRatio": [["i", np.expm1, [1, 0]]],
     "Electron_r9": [["i", np.exp, [1, 1e-2]]],

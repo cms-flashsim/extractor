8af2fea5dfba85df5364dfeafab0c2113d8b1b19
fcattafesta <filippo.cattafesta99@gmail.com>
2023-05-29 11:42:18 +0200

    NaN resampling function

--- trainer/training/electrons/jets/validation.py
+++ trainer/training/electrons/jets/validation.py
@@ -26,6 +26,8 @@ from postprocessing import postprocessing
 from post_actions_jet import target_dictionary
 from corner_plots import make_corner
 
+from nan_resampling import nan_resampling
+
 from electrons.columns import gen_jet as gen_jetM
 from electrons.columns import reco_columns as reco_columnsM
 
@@ -85,6 +87,10 @@ def validate(
     gen = np.array(gen).reshape((-1, args.y_dim))
     reco = np.array(reco).reshape((-1, args.x_dim))
     samples = np.array(samples).reshape((-1, args.x_dim))
+
+    samples = nan_resampling(samples, gen, model)
+
+    # check nan in samples
 
     fullarray = np.concatenate((gen, reco, samples), axis=1)
     full_sim_cols = ["Full_" + x for x in reco_columns]
---
+++ trainer/utils/nan_resampling.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch
+
+
+def nan_resampling(sample, gen, model):
+    sample = torch.tensor(sample)
+    gen = torch.tensor(gen)
+    nan_mask = torch.isnan(sample).any(axis=0)
+    if nan_mask.any():
+        nan_idx = torch.argwhere(nan_mask)
+        # Generate new samples
+        model.eval()
+        model.to("cpu")
+        while True:
+            with torch.no_grad():
+                sample[nan_idx] = model.sample(num_samples=1, context=gen[nan_mask])
+                if not torch.isnan(sample[nan_idx]).any():
+                    break
+    sample = sample.numpy()
+    return sample

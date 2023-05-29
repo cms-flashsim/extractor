7103e1e6c24a396e1590bd3272bc220e0f124806
fcattafesta <filippo.cattafesta99@gmail.com>
2023-05-29 14:21:21 +0200

    Debug

--- trainer/utils/nan_resampling.py
+++ trainer/utils/nan_resampling.py
@@ -3,19 +3,18 @@ import torch
 
 
 def nan_resampling(sample, gen, model, device):
-    sample = torch.tensor(sample)
-    gen = torch.tensor(gen)
+    sample = torch.tensor(sample).to(device)
+    gen = torch.tensor(gen).to(device)
     nan_mask = torch.isnan(sample).any(axis=1)
     if nan_mask.any():
         nan_idx = torch.argwhere(nan_mask)
         # Generate new samples
         model.eval()
-        model.to("cpu")
         while True:
             with torch.no_grad():
                 sample[nan_idx] = model.sample(num_samples=1, context=gen[nan_mask])
                 if not torch.isnan(sample[nan_idx]).any():
                     break
-    model.to(device)
-    sample = sample.numpy()
+    sample = sample.detach().cpu().numpy()
+    gen = gen.detach().cpu().numpy()
     return sample

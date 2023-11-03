77e831afa22c3e8185e4ff22d65554139b048d29
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-11-03 12:21:10 +0100

    oversample dataset

---
+++ trainer/extractor/fatjets/columns.py
@@ -0,0 +1,19 @@
+gen = [
+    "MgenjetAK8_pt",
+    "MgenjetAK8_phi",
+    "MgenjetAK8_eta",
+    "MgenjetAK8_hadronFlavour",
+    "MgenjetAK8_partonFlavour",
+    "MgenjetAK8_mass",
+    "MgenjetAK8_ncFlavour",
+    "MgenjetAK8_nbFlavour",
+    "is_signal",
+    "has_H_within_0_8",]
+
+reco = [
+    "Mpt_ratio",
+    "Meta_sub",
+    "Mphi_sub",
+    "Mfatjet_msoftdrop",
+    "Mfatjet_particleNetMD_XbbvsQCD",
+]
--- trainer/extractor/fatjets/training_dataset.py
+++ trainer/extractor/fatjets/training_dataset.py
@@ -4,8 +4,56 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "preprocessing"))
 
 from preprocessing import preprocessing
-from prep_actions import target_dictionary_jets as target_dictionary
-from columns import jet_cond, reco_columns
+from columns import gen, reco
+
+import pandas as pd
+import numpy as np
 
 if __name__ == "__main__":
-    print("to be implemented")
+    
+    df = pd.read_pickle("~/Downloads/preprocessed_concat.pkl")
+
+    # Define the exact columns to keep in the specified order
+    columns_to_keep_exact = [
+        "MgenjetAK8_pt",
+        "MgenjetAK8_phi",
+        "MgenjetAK8_eta",
+        "MgenjetAK8_hadronFlavour",
+        "MgenjetAK8_partonFlavour",
+        "MgenjetAK8_mass",
+        "MgenjetAK8_ncFlavour",
+        "MgenjetAK8_nbFlavour",
+        "has_H_within_0_8",
+        "is_signal",
+        "Mpt_ratio",
+        "Meta_sub",
+        "Mphi_sub",
+        "Mfatjet_msoftdrop",
+        "Mfatjet_particleNetMD_XbbvsQCD",
+    ]
+
+    # Select only the specified columns from the dataframe
+    df_selected = df[columns_to_keep_exact]
+
+    signal = df_selected[df_selected['is_signal'] == 1].values
+    background = df_selected[df_selected['is_signal'] == 0].values
+
+    # oversample the signal
+    signal = np.repeat(signal, 15, axis=0)
+
+    # concatenate the signal and background
+    data = np.concatenate([signal, background])
+
+    # shuffle the data
+    np.random.shuffle(data)
+
+    # get back to pandas dataframe
+    df = pd.DataFrame(data, columns=columns_to_keep_exact)
+    # print stats
+    print(df.head())
+    print(df.describe())
+    # save the dataframe
+    df.to_pickle("preprocessed_concat_oversampled.pkl")
+
+
+

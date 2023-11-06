83bea3729b71f6a7d14e27b1393acb250c6fb1a0
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-11-06 15:18:44 +0100

    add fat jets

--- trainer/extractor/fat_jets/training_dataset.py
+++ trainer/extractor/fat_jets/training_dataset.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "preprocessing"))
 
@@ -10,7 +11,7 @@ from columns import gen, reco
 if __name__ == "__main__":
 
     print("Starting to make dataset for fake jets")
-    df = pd.read_pickle("/home/fvaselli/Downloads/preprocessed_concat(1).pkl")
+    df = pd.read_pickle("/home/fvaselli/Downloads/preprocessed_QCD_noPU_VBF_SM.pkl")
 
     # Define the exact columns to keep in the specified order
     columns_to_keep_exact = [
@@ -38,18 +39,27 @@ if __name__ == "__main__":
     print("df_selected shape: ", df_selected.shape)
     print(df_selected.head())
 
+    # plot histogram of Mfatjet_particleNetMD_XbbvsQCD
+    plt.hist(df_selected["Mfatjet_particleNetMD_XbbvsQCD"], bins=100)
+    plt.show()
+    
     # oversample the signal
     sig_values = df_selected[df_selected["is_signal"] == 1].values
-    sig_values = sig_values.repeat(15, axis=0)
+    sig_values = sig_values.repeat(20, axis=0)
 
     # concat
     df_selected = pd.concat([df_selected, pd.DataFrame(sig_values, columns=df_selected.columns)])
-    # shuffle
+    # shuffle df
     df_selected = df_selected.sample(frac=1).reset_index(drop=True)
+    # print is_signal values
+    # print(df_selected["is_signal"].values[45:70])
 
     # print info
     print("df_selected shape: ", df_selected.shape)
     print(df_selected.head())
 
+    # plot histogram of Mfatjet_particleNetMD_XbbvsQCD
+    plt.hist(df_selected["Mfatjet_particleNetMD_XbbvsQCD"], bins=100)
+    plt.show()
     # save
     # df_selected.to_pickle("fatjet_oversampled.pkl")
